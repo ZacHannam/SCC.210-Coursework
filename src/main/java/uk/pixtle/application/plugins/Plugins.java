0de51c783b86eb7d1088ad64d0fad86dd0993e19
Orme, Louis (Student) <l.e.orme@lancaster.ac.uk>
2022-12-13 13:04:58 +0000

    Added a historyViewer plugin, with a method to add to the list. TODO, make undo button smaller

--- src/main/java/uk/pixtle/application/plugins/Plugins.java
+++ src/main/java/uk/pixtle/application/plugins/Plugins.java
@@ -9,8 +9,10 @@ public enum Plugins {
     //EXAMPLE(ExamplePlugin.class),
     ColourPalette(ColourPalettePlugin.class),
     HexPlugin(HexPlugin.class),
+    RGBPlugin(uk.pixtle.application.plugins.plugins.RGBPlugin.class),
     ColourPreviewPlugin(uk.pixtle.application.plugins.plugins.ColourPreviewPlugin.class),
-    RGBPlugin(uk.pixtle.application.plugins.plugins.RGBPlugin.class);
+    HistoryPlugin(uk.pixtle.application.plugins.plugins.HistoryPlugin.class);
+
     ;
 
     @Getter
