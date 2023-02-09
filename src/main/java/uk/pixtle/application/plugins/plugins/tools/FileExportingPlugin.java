627a745821561c3261be73978f9250084302b1b6
Louis <louis.orme123@gmail.com>
2023-02-09 18:46:17 +0000

    Final exporting of image, works with drawing and image layers, and background colour. Pair Programming with Harry.

--- src/main/java/uk/pixtle/application/plugins/plugins/tools/FileExportingPlugin.java
+++ src/main/java/uk/pixtle/application/plugins/plugins/tools/FileExportingPlugin.java
@@ -8,7 +8,9 @@ import uk.pixtle.application.plugins.plugins.canvas.infinitecanvas.layer.Layer;
 import uk.pixtle.application.plugins.plugins.canvas.infinitecanvas.layer.LayerManager;
 import uk.pixtle.application.plugins.plugins.canvas.infinitecanvas.layer.LayerType;
 import uk.pixtle.application.plugins.plugins.canvas.infinitecanvas.layer.drawinglayer.DrawingLayer;
+import uk.pixtle.application.plugins.plugins.canvas.infinitecanvas.layer.drawinglayer.DrawingLayerImageProcessor;
 import uk.pixtle.application.plugins.plugins.canvas.infinitecanvas.layer.imagelayer.ImageLayer;
+import uk.pixtle.application.plugins.plugins.canvas.infinitecanvas.layer.imagelayer.ImageLayerImageProcessor;
 import uk.pixtle.application.ui.window.minitoollist.MiniToolPanel;
 
 import javax.imageio.ImageIO;
@@ -20,6 +22,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.awt.image.*;
 
@@ -40,14 +43,95 @@ public class FileExportingPlugin extends ToolPlugin implements PluginMiniToolExp
 
     }
 
-    public void exportFile(String PATH){
-        //final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        BufferedImage image;
+    public void exportFile(String PATH)
+    {
+        InfiniteCanvasPlugin infiniteCanvas = (InfiniteCanvasPlugin) super.getApplication().getPluginManager().getActiveCanvasPlugin();
+        LayerManager lm = infiniteCanvas.getLayerManager();
+        ArrayList<Layer> layers = lm.getLayers();
+        Collections.reverse(layers);
 
+        final int[] biggestHeight = {0};
+        final int[] biggestWidth = {0};
+
+        layers.forEach((l) -> {
+            switch (l.getLayerType()) {
+                case DRAWING:
+                    DrawingLayer currentLayer = (DrawingLayer) l;
+                    DrawingLayerImageProcessor dlip = new DrawingLayerImageProcessor(currentLayer);
+                    BufferedImage layerAsImage = dlip.getLayerAsBufferedImage();
+                    if (layerAsImage.getHeight() > biggestHeight[0])
+                        biggestHeight[0] = layerAsImage.getHeight();
+                    if (layerAsImage.getWidth() > biggestWidth[0])
+                        biggestWidth[0] = layerAsImage.getWidth();
+                    break;
+                case IMAGE:
+                    ImageLayer currentLayerImage = (ImageLayer) l;
+                    ImageLayerImageProcessor ilip = new ImageLayerImageProcessor(currentLayerImage);
+                    BufferedImage layerAsImageimg = ilip.getLayerAsBufferedImage();
+                    if (layerAsImageimg.getHeight() > biggestHeight[0])
+                        biggestHeight[0] = layerAsImageimg.getHeight();
+                    if (layerAsImageimg.getWidth() > biggestWidth[0])
+                        biggestWidth[0] = layerAsImageimg.getWidth();
+                    break;
+                default:
+                    break;
+            }
+
+        });
+
+        BufferedImage newImg = new BufferedImage(biggestWidth[0],biggestHeight[0],BufferedImage.TYPE_INT_ARGB | BufferedImage.SCALE_FAST);
+        Graphics2D g2 = (Graphics2D) newImg.createGraphics();
+        g2.setPaint(infiniteCanvas.getBackgroundColor());
+        g2.fillRect(0,0,biggestWidth[0], biggestHeight[0]);
+
+        layers.forEach((l) -> {
+            switch (l.getLayerType()) {
+                case DRAWING:
+                    DrawingLayer currentLayer = (DrawingLayer)l;
+                    DrawingLayerImageProcessor dlip = new DrawingLayerImageProcessor(currentLayer);
+                    BufferedImage layerAsImage = dlip.getLayerAsBufferedImage();
+                    g2.drawImage(layerAsImage, null, 0 ,0);
+                    break;
+                case IMAGE:
+                    ImageLayer currentLayerImage = (ImageLayer) l;
+                    ImageLayerImageProcessor ilip = new ImageLayerImageProcessor(currentLayerImage);
+                    BufferedImage layerAsImageimg = ilip.getLayerAsBufferedImage();
+                    g2.drawImage(layerAsImageimg,null,0,0);
+                    break;
+                default:
+                    break;
+                }
+
+        });
+
+        try
+        {
+            ImageIO.write(newImg, "PNG", new File(PATH));
+        }
+        catch (Exception e)
+        {
+            System.out.print(e);
+        }
+    }
+
+    public void oldExport(String PATH){
+        //final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final BufferedImage[] image = new BufferedImage[1];
+        final DrawingLayerImageProcessor[] dlip = new DrawingLayerImageProcessor[1];
         InfiniteCanvasPlugin infiniteCanvas = (InfiniteCanvasPlugin) super.getApplication().getPluginManager().getActiveCanvasPlugin();
         LayerManager lm = infiniteCanvas.getLayerManager();
 
         ArrayList<Layer> layers = lm.getLayers();
+
+        BufferedImage newImg = new BufferedImage(2000,2000,BufferedImage.TYPE_INT_ARGB | BufferedImage.SCALE_FAST);
+        Graphics2D grap = (Graphics2D) newImg.createGraphics();
+        DrawingLayerImageProcessor dlip2 = new DrawingLayerImageProcessor(layers.get(0));
+        grap.drawImage(dlip2.getLayerAsBufferedImage(), null, 0,0);
+        dlip2 = new DrawingLayerImageProcessor(layers.get(1));
+        grap.drawImage(dlip2.getLayerAsBufferedImage(), null, 0,0);
+
+
+
         System.out.print(layers);
         try{
             layers.forEach((l) -> {
@@ -59,8 +143,12 @@ public class FileExportingPlugin extends ToolPlugin implements PluginMiniToolExp
                 switch (l.getLayerType()) {
                     case DRAWING:
                         DrawingLayer currentLayer = (DrawingLayer)l;
-                        HashMap chunkMap = currentLayer.getChunkMap();
-                        chunkMap.forEach((s, c) -> {
+                        //dlip[0] = new DrawingLayerImageProcessor(currentLayer);
+                        //image[0] = new BufferedImage(1000,1000, BufferedImage.TYPE_INT_ARGB | BufferedImage.SCALE_FAST);
+                        //Graphics2D g2 = (Graphics2D) image[0].createGraphics();
+                        //g2.drawImage(dlip[0].getLayerAsBufferedImage(), null, 0, 0);
+                        HashMap chunkMap = currentLayer.getChunkMap(); //Get the chunk map of the current layer
+                        chunkMap.forEach((s, c) -> { //Loop through the current chunk map
                             //System.out.printf("Current chunk - %s : %s\n",s,c);
                             //System.out.printf("Current String working on: %s\n", s);
                             String[] ChunkString = s.toString().split(":");
@@ -110,6 +198,14 @@ public class FileExportingPlugin extends ToolPlugin implements PluginMiniToolExp
             System.out.print(e);
         }
 
+        try
+        {
+            ImageIO.write(newImg, "PNG", new File(PATH));
+        }
+        catch (Exception e)
+        {
+            System.out.print(e);
+        }
 
 
     }
