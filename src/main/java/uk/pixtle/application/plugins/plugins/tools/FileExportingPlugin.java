60646893373c4c0c42dbc9e4fec0e176ad52da81
ormele <ormele@lancaster.ac.uk>
2023-02-13 09:24:16 +0000

    updated method using zac's stuff

--- src/main/java/uk/pixtle/application/plugins/plugins/tools/FileExportingPlugin.java
+++ src/main/java/uk/pixtle/application/plugins/plugins/tools/FileExportingPlugin.java
@@ -45,7 +45,36 @@ public class FileExportingPlugin extends ToolPlugin implements PluginMiniToolExp
 
     public void exportFile(String PATH)
     {
+        //if layer is hidden code
+        System.out.print(PATH);
         InfiniteCanvasPlugin infiniteCanvas = (InfiniteCanvasPlugin) super.getApplication().getPluginManager().getActiveCanvasPlugin();
+
+        BufferedImage finalImg = infiniteCanvas.getFullImage(false);
+        BufferedImage newImg = new BufferedImage(finalImg.getWidth(),finalImg.getHeight(),BufferedImage.TYPE_INT_ARGB | BufferedImage.SCALE_FAST);
+
+        Graphics2D g2 = (Graphics2D) newImg.createGraphics();
+        if(!Objects.equals(infiniteCanvas.getBackgroundColor(), new Color(255, 255, 255)))
+        {
+            g2.setPaint(infiniteCanvas.getBackgroundColor());
+            g2.fillRect(0,0,finalImg.getWidth(), finalImg.getHeight());
+        }
+
+        g2.drawImage(finalImg,null,0,0);
+        try
+        {
+            ImageIO.write(newImg, "PNG", new File(PATH));
+        }
+        catch (Exception e)
+        {
+            System.out.print(e);
+        }
+    }
+
+    public void oldExport(String PATH){
+        /*
+        InfiniteCanvasPlugin infiniteCanvas = (InfiniteCanvasPlugin) super.getApplication().getPluginManager().getActiveCanvasPlugin();
+        BufferedImage finalImg = infiniteCanvas.getFullImage(false);
+        BufferedImage newImg = new BufferedImage(finalImg.getWidth(),finalImg.getHeight(),BufferedImage.TYPE_INT_ARGB | BufferedImage.SCALE_FAST);
         LayerManager lm = infiniteCanvas.getLayerManager();
         ArrayList<Layer> layers = lm.getLayers();
         Collections.reverse(layers);
@@ -119,109 +148,10 @@ public class FileExportingPlugin extends ToolPlugin implements PluginMiniToolExp
                     g2.drawImage(layerAsImagetxt, null,0,0);
                 default:
                     break;
-                }
+            }
 
         });
-
-        try
-        {
-            ImageIO.write(newImg, "PNG", new File(PATH));
-        }
-        catch (Exception e)
-        {
-            System.out.print(e);
-        }
-    }
-
-    public void oldExport(String PATH){
-        //final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        final BufferedImage[] image = new BufferedImage[1];
-        final DrawingLayerImageProcessor[] dlip = new DrawingLayerImageProcessor[1];
-        InfiniteCanvasPlugin infiniteCanvas = (InfiniteCanvasPlugin) super.getApplication().getPluginManager().getActiveCanvasPlugin();
-        LayerManager lm = infiniteCanvas.getLayerManager();
-
-        ArrayList<Layer> layers = lm.getLayers();
-
-        BufferedImage newImg = new BufferedImage(2000,2000,BufferedImage.TYPE_INT_ARGB | BufferedImage.SCALE_FAST);
-        Graphics2D grap = (Graphics2D) newImg.createGraphics();
-        DrawingLayerImageProcessor dlip2 = new DrawingLayerImageProcessor(layers.get(0));
-        grap.drawImage(dlip2.getLayerAsBufferedImage(), null, 0,0);
-        dlip2 = new DrawingLayerImageProcessor(layers.get(1));
-        grap.drawImage(dlip2.getLayerAsBufferedImage(), null, 0,0);
-
-
-        try{
-            layers.forEach((l) -> {
-                final int[] smallestX = { 999999999 };
-                final int[] biggestX = { 0 };
-                final int[] smallestY = { 999999999 };
-                final int[] biggestY = { 0 };
-                switch (l.getLayerType()) {
-                    case DRAWING:
-                        DrawingLayer currentLayer = (DrawingLayer)l;
-                        //dlip[0] = new DrawingLayerImageProcessor(currentLayer);
-                        //image[0] = new BufferedImage(1000,1000, BufferedImage.TYPE_INT_ARGB | BufferedImage.SCALE_FAST);
-                        //Graphics2D g2 = (Graphics2D) image[0].createGraphics();
-                        //g2.drawImage(dlip[0].getLayerAsBufferedImage(), null, 0, 0);
-                        HashMap chunkMap = currentLayer.getChunkMap(); //Get the chunk map of the current layer
-                        chunkMap.forEach((s, c) -> { //Loop through the current chunk map
-                            //System.out.printf("Current chunk - %s : %s\n",s,c);
-                            //System.out.printf("Current String working on: %s\n", s);
-                            String[] ChunkString = s.toString().split(":");
-                            int currentX = Integer.parseInt(ChunkString[0]);
-                            int currentY = Integer.parseInt(ChunkString[1]);
-                            //System.out.printf("%d : %d\n", currentX, currentY);
-                            if(currentX> biggestX[0])
-                                biggestX[0] = currentX;
-
-                            if(currentX< smallestX[0])
-                                smallestX[0] = currentX;
-
-                            if(currentY > biggestY[0])
-                                biggestY[0] = currentY;
-
-                            if(currentY < smallestY[0])
-                                smallestY[0] = currentY;
-
-
-                        });
-                        int topPixelY = smallestY[0] * currentLayer.getPixelsPerChunk();
-                        int topPixelX = smallestX[0] * currentLayer.getPixelsPerChunk();
-                        int bottomPixelY = (biggestY[0]+1)  * (currentLayer.getPixelsPerChunk() - 1);
-                        int bottomPixelX = (biggestX[0]+1)  * (currentLayer.getPixelsPerChunk() - 1);
-                        break;
-                    case IMAGE:
-                        ImageLayer currentLayerImage = (ImageLayer) l;
-                        Point topLeftPixel = currentLayerImage.getTopLeftPixel();
-                        Point bottomRightPixel = currentLayerImage.getBottomRightPixel();
-                        int height=bottomRightPixel.y - topLeftPixel.y;
-                        int width=bottomRightPixel.x - topLeftPixel.x;
-                        //image = new BufferedImage(width,height, BufferedImage.TYPE_INT_ARGB | BufferedImage.SCALE_FAST); //won't work as needs to be final for lambda.
-                        break;
-                    default:
-
-                        break;
-                }
-                //System.out.printf("Big and small x %d : %d\n", biggestX[0], smallestX[0]);
-                //System.out.printf("Big and small y %d : %d\n", biggestY[0], smallestY[0]);
-            });
-
-        }
-        catch (Exception e)
-        {
-            System.out.print(e);
-        }
-
-        try
-        {
-            ImageIO.write(newImg, "PNG", new File(PATH));
-        }
-        catch (Exception e)
-        {
-            System.out.print(e);
-        }
-
-
+*/
     }
 
 
